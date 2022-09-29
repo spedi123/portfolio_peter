928c8f9eb9b275456edbf260658b813263948578
spedi123 <peteran8604@gmail.com>
2022-09-29 14:00:33 -0700

    template for about page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {Header} from './components/Header'
 import {Home} from './components/Home'
+import {About} from './components/About'
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <About />
     </div>
   );
 }
---
+++ portfolio/src/components/About.js
@@ -0,0 +1,8 @@
+export const About = (props) => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default About;
